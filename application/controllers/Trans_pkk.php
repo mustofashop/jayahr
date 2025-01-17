5df155e4fcc41e06333b580c0c0e12f87659fe98
Mustofa <mustofaa1802@gmail.com>
2025-01-17 13:35:18 +0700

    tambah controller baru

---
+++ application/controllers/Trans_pkk.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+require('./vendor/autoload.php');
+
+class Trans_pkk extends CI_Controller
+{
+    public function save_set_pkk()
+    {
+        $masuk  = $this->session->userdata('masuk_k');
+        if ($masuk != TRUE) {
+            redirect(base_url('login'));
+        } else {
+            // $dt['id_trans_pkk']     = $this->input->post('id_trans_pkk');
+            $dt['id_periode']       = $this->input->post('id_periode');
+            $dt['id_jenis_form']    = $this->input->post('id_jenis_form');
+            $dt['id_p_periode']     = $this->input->post('id_p_periode');
+            $dt['nrp']              = $this->input->post('nrp');
+            $dt['insert_by']        = $this->session->userdata('nrp');
+            $dt['insert_date']      = date("Y-m-d");
+            $this->db->insert("trans_pkk", $dt);
+
+            $this->session->set_flashdata('msg', 'Pengaturan Sukses disimpan');
+            redirect($_SERVER['HTTP_REFERER']);
+        }
+    }
+}
